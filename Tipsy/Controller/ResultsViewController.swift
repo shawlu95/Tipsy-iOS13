4e149068cdc1415020b13f5507cc13ade1b8a2e5
Shaw Lu <shawlu95@gmail.com>
2023-06-09 23:44:21 -0700

    create ResultsViewController

---
+++ Tipsy/Controller/ResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RerulstsViewController.swift
+//  Tipsy
+//
+//  Created by main on 6/9/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settomgsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+}
